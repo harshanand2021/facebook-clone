b8d438fe1d4c516449cc939e6de8bd08673efaa8
Harsh Anand <harsh250600@gmail.com>
2023-09-15 13:23:05 +0530

    facebook-clone react app initialized

---
+++ src/LoginPage/LoginPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./LoginPage.css";
+
+function LoginPage() {
+  return (
+    <div className="Login">
+      <div className="Facebook">
+        <div className="facebook-text">facebook</div>
+        <div className="title">
+          Facebook helps you connect and share <br/> with the people in your
+          life.
+        </div>
+      </div>
+      <div className="login-container">
+        <div className="login-details">
+            <input type="email" placeholder="Email address or Phone number" /><br />
+            <input type="password" placeholder="Password" />
+            <br />
+            <br />
+            <button className="btn">
+                Login
+            </button>
+            <div className="forgotten-password">
+                <a href="forget">Forgotten Password</a>
+                <br />
+            </div>
+            <div className="create">
+                <br /><br />
+                <button className="btns">
+                    Create New Account
+                </button>
+            </div>
+            <p></p>
+            <br />
+            <div className="Page">
+                <a href="createPage">Create a Page</a> for a celebrity, brand or business.
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default LoginPage;
