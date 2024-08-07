c7aea3c0cd5ae36123106bd795cb3032fca8ff1a
Harsh Anand <harsh250600@gmail.com>
2024-08-07 13:35:32 +0530

    firebase configuration added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnwi7Ez6MgI0TaXG6sjkAMMo0S6QRMxkM",
+  authDomain: "facebook-clone-1f4ba.firebaseapp.com",
+  projectId: "facebook-clone-1f4ba",
+  storageBucket: "facebook-clone-1f4ba.appspot.com",
+  messagingSenderId: "100553250510",
+  appId: "1:100553250510:web:b2cbb8008038fdb4b2629f",
+  measurementId: "G-5J8853EZ2D",
+};
